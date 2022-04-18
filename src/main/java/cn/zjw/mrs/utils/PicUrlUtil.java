577055bc7fbf46af34dfbbabb628f24e36f36602
Vanish-Zeng <957589005@qq.com>
2022-04-19 00:28:19 +0800

    图像完整url拼接工具类。🧣

---
+++ src/main/java/cn/zjw/mrs/utils/PicUrlUtil.java
@@ -0,0 +1,39 @@
+package cn.zjw.mrs.utils;
+
+import org.apache.logging.log4j.util.Strings;
+
+/**
+ * @Classname PicUrlUtil
+ * @Date 2022/4/18 13:30
+ * @Created by zjw
+ * @Description
+ */
+public class PicUrlUtil {
+    /**
+     * 获取完整图片路径
+     * 由于数据库中的图片url路径为oss存储路径的后缀，所以返回给前端前需要将路径补全
+     * @param avatar 头像路径后缀
+     * @return 完整头像url
+     */
+    public static String getFullAvatarUrl(String avatar) {
+        String avatarUrl = null;
+        if (Strings.isNotBlank(avatar)) {
+            avatarUrl = "https://mrs-zjw.oss-cn-hangzhou.aliyuncs.com/mrs/avatar/" + avatar;
+        }
+        return avatarUrl;
+    }
+
+    /**
+     * 获取完整图片路径
+     * 由于数据库中的图片url路径为oss存储路径的后缀，所以返回给前端前需要将路径补全
+     * @param pic 电影海报路径后缀
+     * @return 完整电影海报url
+     */
+    public static String getFullMoviePicUrl(String pic) {
+        String avatarUrl = null;
+        if (Strings.isNotBlank(pic)) {
+            avatarUrl = "https://mrs-zjw.oss-cn-hangzhou.aliyuncs.com/mrs/movie/" + pic;
+        }
+        return avatarUrl;
+    }
+}
